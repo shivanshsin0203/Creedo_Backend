7abe3cf35873287166f5dc12091e001600001ad0
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-29 20:05:48 +0530

    Added Friend Request logic

--- index.js
+++ index.js
@@ -23,21 +23,71 @@ const UserSchema = new mongoose.Schema({
   email: String,
   id: String,
 });
-
+const FriendRequestSchema = new mongoose.Schema({
+  to: String,
+  from: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 const User = mongoose.model("user", UserSchema);
+const FrRequest = mongoose.model("friendrequest", FriendRequestSchema);
+const isValidEmail = (email) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+async function checkFriendRequest(email) {
+  if (!isValidEmail(email.to)) {
+    return "Invalid_Email";
+  }
+  console.log(isValidEmail(email.to) + "email");
+  const UserPresent = await User.findOne({ email: email.to });
+  if (!UserPresent) {
+    return "User_Not_Found";
+  }
+  if (email.from === email.to) {
+    return "Same_Email";
+  }
+  const result = await FrRequest.create(email);
+  return "Success";
+}
 
 app.post("/register", (req, res) => {
   console.log(req.body);
   const user = new User(req.body);
   async function saveUser() {
-    const present = await User.findOne({ email: user.email })
-    if(present) {
-    return console.log("User already exists");}
-    else{
-    const result = await User.create(user);}
+    const present = await User.findOne({ email: user.email });
+    if (present) {
+      return console.log("User already exists");
+    } else {
+      const result = await User.create(user);
+    }
   }
   saveUser();
   res.send({ message: "Data received" });
+});
+app.post("/addfriend", (req, res) => {
+  async function check() {
+    console.log(req.body);
+    const result = await checkFriendRequest(req.body);
+    console.log(result + "result");
+    if (result === "Invalid_Email") {
+      return res.json({ message: "Invalid Email", result: false });
+    }
+    if (result === "User_Not_Found") {
+      return res.json({ message: "User not found", result: false });
+    }
+    if (result === "Same_Email") {
+      return res.json({
+        message: "You cannot send request to yourself",
+        result: false,
+      });
+    }
+    if (result === "Success") {
+      return res.json({ message: "Friend Request Sent", result: true });
+    }
+  }
+  check();
 });
 
 app.listen(3005, async () => {
