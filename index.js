76c4ec0bf227ecafe4c3edd5374019eb4c0e8e2c
Shivansh Singh <singhshivansh12may@gmail.com>
2024-02-12 20:47:18 +0530

    New Routes

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ const PostSchema = new mongoose.Schema({
   title: String,
   discription: String,
   image: [String],
-  likes:{
-    type:Number,
-    default:0
-  }
+  likes: {
+    type: Number,
+    default: 0,
+  },
 });
 const CommnetSchema = new mongoose.Schema({
   postid: String,
@@ -45,27 +45,26 @@ const FriendRequestSchema = new mongoose.Schema({
   to: String,
   from: String,
   name: String,
-  picture: {type:String,
-    default:null},
+  picture: { type: String, default: null },
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
-const ConnectionSchema= new mongoose.Schema({
-  user:String,
-  freind_name:String,
-  freind_picture:{
-    type:String,
-    default:null
+const ConnectionSchema = new mongoose.Schema({
+  user: String,
+  freind_name: String,
+  freind_picture: {
+    type: String,
+    default: null,
   },
-  freind_email:String,
+  freind_email: String,
 });
 const User = mongoose.model("user", UserSchema);
 const FrRequest = mongoose.model("friendrequest", FriendRequestSchema);
-const Connection=mongoose.model("connection",ConnectionSchema);
+const Connection = mongoose.model("connection", ConnectionSchema);
 const Post = mongoose.model("post", PostSchema);
-const Comment= mongoose.model("comment", CommnetSchema);
+const Comment = mongoose.model("comment", CommnetSchema);
 const isValidEmail = (email) => {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 };
@@ -81,11 +80,17 @@ async function checkFriendRequest(email) {
   if (email.from === email.to) {
     return "Same_Email";
   }
-  const IsConnection = await Connection.findOne({freind_email:email.to,user:email.from});
-  if(IsConnection){
+  const IsConnection = await Connection.findOne({
+    freind_email: email.to,
+    user: email.from,
+  });
+  if (IsConnection) {
     return "Already_Friends";
   }
-  const IsRequestSent = await FrRequest.findOne({ to: email.to, from: email.from });
+  const IsRequestSent = await FrRequest.findOne({
+    to: email.to,
+    from: email.from,
+  });
   if (IsRequestSent) {
     return "Request_Already_Sent";
   }
@@ -140,66 +145,82 @@ app.post("/addfriend", (req, res) => {
   check();
 });
 app.post("/getfriendrequests", async (req, res) => {
-  const result= await FrRequest.find({to:req.body.email});
-  res.json({result:result});
+  const result = await FrRequest.find({ to: req.body.email });
+  res.json({ result: result });
 });
-app.post("/acceptfriendrequest",async(req,res)=>{
-  const destroy=await FrRequest.deleteOne({_id:req.body.requestid});
-  const result1=await Connection.create({user:req.body.user1_email,freind_name:req.body.user2_name,freind_picture:req.body.user2_picture,freind_email:req.body.user2_email});
-  const result2=await Connection.create({user:req.body.user2_email,freind_name:req.body.user1_name,freind_picture:req.body.user1_picture,freind_email:req.body.user1_email});
-  res.json({message:"Friend Request Accepted"});
+app.post("/acceptfriendrequest", async (req, res) => {
+  const destroy = await FrRequest.deleteOne({ _id: req.body.requestid });
+  const result1 = await Connection.create({
+    user: req.body.user1_email,
+    freind_name: req.body.user2_name,
+    freind_picture: req.body.user2_picture,
+    freind_email: req.body.user2_email,
+  });
+  const result2 = await Connection.create({
+    user: req.body.user2_email,
+    freind_name: req.body.user1_name,
+    freind_picture: req.body.user1_picture,
+    freind_email: req.body.user1_email,
+  });
+  res.json({ message: "Friend Request Accepted" });
 });
-app.post('/deletefriendrequest',async(req,res)=>{
-  const destroy=await FrRequest.deleteOne({_id:req.body.requestid});
+app.post("/deletefriendrequest", async (req, res) => {
+  const destroy = await FrRequest.deleteOne({ _id: req.body.requestid });
 });
-app.post('/getconnections',async(req,res)=>{
-  const result=await Connection.find({user:req.body.email});
-  res.json({result:result});
+app.post("/getconnections", async (req, res) => {
+  const result = await Connection.find({ user: req.body.email });
+  res.json({ result: result });
 });
-app.post('/createpost',async(req,res)=>{
- 
-  const result=await Post.create(req.body);
-  res.json({message:"Post Created"});
+app.post("/createpost", async (req, res) => {
+  const result = await Post.create(req.body);
+  res.json({ message: "Post Created" });
 });
-app.post('/posts',async(req,res)=>{
-  const result=await Post.find();
+app.post("/posts", async (req, res) => {
+  const result = await Post.find();
   result.reverse();
-   if(result.length<req.body.posts+2)
-   {
-    res.json({data:"No More Posts"});
-   }
-    else{
-      res.json({data:result.slice(req.body.posts,req.body.posts+2)});
-    }
-});
-app.post('/finduser',async(req,res)=>{
-  const result=await User.find({email:req.body.email});
-  res.json({result:result});
-})
-app.post('/updatelike',async(req,res)=>{
-  const result=await Post.findByIdAndUpdate(req.body.id,{$inc:{likes:1}});
-  res.json({message:"Like Updated"});
-})
-app.post('/getpostbyid',async(req,res)=>{
-  const result=await Post.findById(req.body.id);
-  res.json({result:result});
-})
-app.post('/addcomment',async(req,res)=>{
-  const result=await Comment.create(req.body);
-  res.json({message:"Comment Added"});
-})
-app.post('/getcomments',async(req,res)=>{
-  const result=await Comment.find({postid:req.body.postid});
-  res.json({result:result});
-})
-app.post('/isfriend',async(req,res)=>{
-  const result=await Connection.findOne({user:req.body.user,freind_email:req.body.freind});
-  if(result){
-    res.json({result:true});
+  if (result.length < req.body.posts + 2) {
+    res.json({ data: "No More Posts" });
+  } else {
+    res.json({ data: result.slice(req.body.posts, req.body.posts + 2) });
   }
-  else{
-    res.json({result:false});
-  }});
+});
+app.post("/finduser", async (req, res) => {
+  const result = await User.find({ email: req.body.email });
+  res.json({ result: result });
+});
+app.post("/updatelike", async (req, res) => {
+  const result = await Post.findByIdAndUpdate(req.body.id, {
+    $inc: { likes: 1 },
+  });
+  res.json({ message: "Like Updated" });
+});
+app.post("/getpostbyid", async (req, res) => {
+  const result = await Post.findById(req.body.id);
+  res.json({ result: result });
+});
+app.post("/addcomment", async (req, res) => {
+  const result = await Comment.create(req.body);
+  res.json({ message: "Comment Added" });
+});
+app.post("/getcomments", async (req, res) => {
+  const result = await Comment.find({ postid: req.body.postid });
+  res.json({ result: result });
+});
+app.post("/isfriend", async (req, res) => {
+  const result = await Connection.findOne({
+    user: req.body.user,
+    freind_email: req.body.freind,
+  });
+  if (result) {
+    res.json({ result: true });
+  } else {
+    res.json({ result: false });
+  }
+});
+app.post("/getpostbymail", async (req, res) => {
+  const result = await Post.find({ creator: req.body.email });
+  res.json({ result: result });
+});
 app.listen(3005, async () => {
   console.log("Server Started at " + 3005);
   await connect();
